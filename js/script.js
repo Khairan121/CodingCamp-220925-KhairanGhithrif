ec06e87af5e8321b5e7c415e283bf0d547e297ad
Khairan121 <100358266+Khairan121@users.noreply.github.com>
2025-09-24 01:57:49 +0700

    kerangka awal nav+hero

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+ const btn = document.getElementById("menu-btn");
+    const menu = document.getElementById("menu");
+    btn.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+
+    // Hero: ganti nama pake JavaScript
+    const nama = "User"; // <-- lo bisa ganti ini dengan input/user login
+    document.getElementById("welcome-text").textContent = `Halo, ${nama}!`;
